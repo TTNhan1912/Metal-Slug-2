b76b2450192681a2a5dfaea42f9762fb7ca84d55
KhanhDuonggg <117138446+KhanhDuonggg@users.noreply.github.com>
2023-06-02 23:47:56 +0700

    KD

--- Metal Slug 2/Assets/Scripts/Player.cs
+++ Metal Slug 2/Assets/Scripts/Player.cs
@@ -179,11 +179,11 @@ public class Player : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("boom"))
-        {
-            boom++;
-            textBoom.text = boom + "";
-        }
+        //if (collision.gameObject.CompareTag("boom"))
+        //{
+        //    boom++;
+        //    textBoom.text = boom + "";
+        //}
     }
 
     private void lifeCheck()
