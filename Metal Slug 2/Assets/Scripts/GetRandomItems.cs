99bf6180fe10e81e5a7a86b8ffd214f5cc8b2509
ThanhTung060303 <ngthanhtung060303@gmail.com>
2023-06-07 20:56:10 +0700

    tung

---
+++ Metal Slug 2/Assets/Scripts/GetRandomItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetRandomItems : MonoBehaviour
+{
+    public List<GameObject> items;
+    private Vector2 shutdownPosition;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // trúng đạn
+        if (collision.gameObject.CompareTag("BulletPistol"))
+        {
+            // Lưu vị trí của quái vật trước khi bị hủy
+            shutdownPosition = transform.position;
+
+            // Kiểm tra xem danh sách prefab có phần tử hay không
+            if (items.Count > 0)
+            {
+                // Chọn một prefab ngẫu nhiên từ danh sách
+                int randomIndex = Random.Range(0, items.Count);
+                GameObject randomPrefab = items[randomIndex];
+
+                // Tạo một phiên bản của prefab ngẫu nhiên tại vị trí quái vật bị hủy
+                Instantiate(randomPrefab, shutdownPosition, Quaternion.identity);
+            }
+        }
+    }
+    
+}
---
+++ Metal Slug 2/Assets/Scripts/HandleItems.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleItems : MonoBehaviour
+{
+
+    private Collider2D collider;
+    private void Start()
+    {
+        collider = GetComponent<Collider2D>();
+
+        string tag = gameObject.tag;
+
+        // trái cây héo, mất sau 4s nếu ko được ăn
+        if(tag == "item_traicay")
+        {
+            Destroy(gameObject,4);
+        }
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // nhân vật ăn item
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (tag == "item_traicay")
+            {
+                // Xử lý logic cho đối tượng có tag "item_traicay"
+                //mất item
+                Destroy(gameObject);
+                Debug.Log("trai cay");
+            }
+            else if (tag == "item_boom")
+            {
+                // Xử lý logic cho đối tượng có tag "item_boom"
+                //mất item
+                Destroy(gameObject);
+                Debug.Log("boom");
+            }
+            else if (tag == "item_hp")
+            {
+                // Xử lý logic cho đối tượng có tag "item_hp"
+                //mất item
+                Destroy(gameObject);
+                Debug.Log("hp");
+            }
+            else if (tag == "item_sting")
+            {
+                // Xử lý logic cho đối tượng có tag "item_sting"
+                //mất item
+                Destroy(gameObject);
+                Debug.Log("sting");
+            }
+            else if (tag == "item_vang")
+            {
+                // Xử lý logic cho đối tượng có tag "item_vang"
+                //mất item
+                Destroy(gameObject);
+                Debug.Log("vang");
+            }
+            else
+            {
+                // Xử lý logic mặc định cho các đối tượng khác
+                Debug.Log("Default logic");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("nen_dat"))
+        {
+            // Tắt thuộc tính isTrigger khi chạm đất
+            //if (tag == "item_traicay" || tag == "item_boom" || tag == "item_sting"
+            //    || tag == "item_hp" || tag == "item_vang")
+            //{
+                collider.isTrigger = false;
+            //}
+        }
+    }
+}
